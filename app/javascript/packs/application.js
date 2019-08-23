648dd1e1d982303a3123d7d2d3dfd51227a961fe
jbsloop <jbsloop@mersoft.com>
2019-08-23 09:58:26 -0500

    first_commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,22 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+
+// Javascript
+require('bootstrap');
+require('admin-lte');
+
+
+// Styles
+require("admin-lte/plugins/inputmask/min/jquery.inputmask.bundle.min.js");
+require("admin-lte/plugins/fontawesome-free/css/all.min.css");
+require("bootstrap/scss/bootstrap");
+require("admin-lte/dist/css/adminlte.min.css");
+require("../stylesheets/application.scss");
